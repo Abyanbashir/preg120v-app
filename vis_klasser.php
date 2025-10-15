8e2317e24f8526ad38aae1a8baafb46e4c315066
abyan <abyan2006@outlook.com>
2025-10-16 01:13:44 +0200

    endret

--- vis_klasser.php
+++ vis_klasser.php
@@ -1,6 +1,22 @@
-<?php require 'db.php'; $rows=$conn->query("SELECT klassekode,klassenavn,studiumkode FROM klasse ORDER BY klassekode"); ?>
-<h1>Alle klasser</h1><p><a href="index.php">Tilbake</a></p>
-<table border="1" cellpadding="6"><tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
-<?php foreach($rows as $r): ?>
-<tr><td><?=h($r['klassekode'])?></td><td><?=h($r['klassenavn'])?></td><td><?=h($r['studiumkode'])?></td></tr>
-<?php endforeach; ?></table>
+
+<?php require_once __DIR__ . '/db.php'; ?>
+<!doctype html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<h1>Alle klasser</h1>
+<p><a href="index.php">Tilbake</a></p>
+<?php $rows = $conn->query("SELECT klassekode, klassenavn, studiumkode FROM klasse ORDER BY klassekode"); ?>
+<table>
+  <thead>
+    <tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
+  </thead>
+  <tbody>
+    <?php foreach($rows as $r): ?>
+      <tr>
+        <td><?= h($r['klassekode']) ?></td>
+        <td><?= h($r['klassenavn']) ?></td>
+        <td><?= h($r['studiumkode']) ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
