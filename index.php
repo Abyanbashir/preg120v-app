6be79e5e42c3b602b7092fd377234f4b0be1afa0
abyan <abyan2006@outlook.com>
2025-10-15 23:51:38 +0200

    endret

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 </head>
 <body>
     <h1>Velkommen til PRG120V-appen</h1>
-    <p>Velg en handling nedenfor:</p>
+    <p>Brukerfunksjoner:</p>
 
     <ul>
-        <li><a href="registrer_klasse.php">Registrer ny klasse</a></li>
+        <li><a href="registrer_klasse.php">Registrer klasse</a></li>
         <li><a href="vis_klasser.php">Vis alle klasser</a></li>
         <li><a href="slett_klasse.php">Slett klasse</a></li>
         <br><br> <!--Her kommer mellomrommet mellom klassene og studentene-->
-        <li><a href="registrer_student.php">Registrer ny student</a></li>
+        <li><a href="registrer_student.php">Registrer student</a></li>
         <li><a href="vis_studenter.php">Vis alle studenter</a></li>
         <li><a href="slett_student.php">Slett student</a></li>
     </ul>
