eabcf960ff5fa423a651859de1dbae1d321ea889
abyan <abyan2006@outlook.com>
2025-10-15 23:33:18 +0200

    endret

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
         <li><a href="registrer_klasse.php">Registrer ny klasse</a></li>
         <li><a href="vis_klasser.php">Vis alle klasser</a></li>
         <li><a href="slett_klasse.php">Slett klasse</a></li>
-        
         <li><a href="registrer_student.php">Registrer ny student</a></li>
         <li><a href="vis_studenter.php">Vis alle studenter</a></li>
         <li><a href="slett_student.php">Slett student</a></li>
