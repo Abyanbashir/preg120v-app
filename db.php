4a28838ffb09e6b315e50dc2273dfaeb8b7da76a
abyan <abyan2006@outlook.com>
2025-10-16 00:23:23 +0200

    endret

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "b-studentsql-1.usn.no";
 $user = "abali4926"; // brukernavnet ditt
-$pass = "DITT_PASSORD"; // skriv inn passordet ditt her
+$pass = "77e5abali4926"; // skriv inn passordet ditt her
 $db   = "abali4926"; // databasenavn (samme som brukernavnet ditt)
 
 // Koble til databasen
