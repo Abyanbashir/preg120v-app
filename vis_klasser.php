f9539e9f061182a14bb15daacc9611a1127bb0c5
abyan <abyan2006@outlook.com>
2025-10-18 23:17:25 +0200

    endret

--- vis_klasser.php
+++ vis_klasser.php
@@ -1,11 +1,23 @@
-<?php require 'db.php'; $res=$conn->query("SELECT * FROM klasse ORDER BY klassekode"); ?>
-<!doctype html><meta charset="utf-8"><link rel="stylesheet" href="style.css"><h1>Alle klasser</h1>
+<?php require_once __DIR__ . '/db.php'; ?>
+<!doctype html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<h1>Alle klasser</h1>
 <p><a href="index.php">Tilbake</a></p>
+<?php
+  $rows = $conn->query("SELECT klassekode, klassenavn, studiumkode FROM klasse ORDER BY klassekode");
+?>
 <table>
-  <thead><tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr></thead>
+  <thead>
+    <tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
+  </thead>
   <tbody>
-    <?php foreach($res as $r): ?>
-      <tr><td><?=h($r['klassekode'])?></td><td><?=h($r['klassenavn'])?></td><td><?=h($r['studiumkode'])?></td></tr>
+    <?php foreach($rows as $r): ?>
+      <tr>
+        <td><?= h($r['klassekode']) ?></td>
+        <td><?= h($r['klassenavn']) ?></td>
+        <td><?= h($r['studiumkode']) ?></td>
+      </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
